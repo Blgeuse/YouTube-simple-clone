b4b9c07cdfafc2da5b59a13838760d95010068b7
Blgeuse <akhmed.r4mazanov@yandex.ru>
2022-05-16 16:37:47 +0300

    Components for a compact sidebar

---
+++ src/components/TheSidebarCompact/SidebarCompactNavItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <a
+      href="#"
+      class="flex flex-col items-center px-2 py-5 hover:bg-gray-100"
+      :class="isActive ? 'text-red-500' : ''"
+    >
+      <BaseIcon class="w-6 h-6 mb-1.5" :name="icon" />
+      <span class="text-xs">{{ title }}</span>
+    </a>
+  </li>
+</template>
+
+<script>
+import BaseIcon from "../BaseIcon.vue";
+export default {
+  props: {
+    icon: String,
+    title: {
+      type: String,
+      default: "Title",
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  components: {
+    BaseIcon,
+  },
+};
+</script>
+
+<style></style>
