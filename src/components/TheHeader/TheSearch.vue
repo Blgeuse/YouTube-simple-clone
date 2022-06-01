f683be82ad579672f8a0988936e63cf157299172
Blgeuse <akhmed.r4mazanov@yandex.ru>
2022-06-01 17:16:38 +0300

    Component for search results

--- src/components/TheHeader/TheSearch.vue
+++ src/components/TheHeader/TheSearch.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="mr-2 flex w-full">
-    <TheSearchInput />
+    <div class="relative flex w-full">
+      <TheSearchInput />
+      <TheSearchResults :results="keywords" />
+    </div>
     <TheSearchButton />
   </div>
 </template>
@@ -8,10 +11,32 @@
 <script>
 import TheSearchInput from "./TheSearchInput.vue";
 import TheSearchButton from "./TheSearchButton.vue";
+import TheSearchResults from "./TheSearchResults.vue";
 export default {
   components: {
     TheSearchInput,
+    TheSearchResults,
     TheSearchButton,
+  },
+  data() {
+    return {
+      keywords: [
+        "new yourk giant",
+        "new yourk alicia keys",
+        "new yourk ginats vs washington football",
+        "new yourk",
+        "new yourk song",
+        "new yourk new yourk frank sinatra",
+        "new yourk jets",
+        "new yourk city",
+        "new yourk giants live",
+        "new yourk state of mind",
+        "new yourk giants vs washington footbal live",
+        "new yourk giants injury",
+        "new yourk giants live stream",
+        "new yourk accent",
+      ],
+    };
   },
 };
 </script>
---
+++ src/components/TheHeader/TheSearchResults.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :class="classes">
+    <ul>
+      <li :class="itemClasses" v-for="result in results" :key="result">
+        {{ result }}
+      </li>
+    </ul>
+    <a :class="reportLinkClasses" herf="#">Report search predictions</a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["results"],
+  computed: {
+    classes() {
+      return [
+        "absolute",
+        "top-full",
+        "w-full",
+        "bg-white",
+        "border",
+        "border-t-0",
+        "border-gray-300",
+        "shadow-md",
+        "pt-4",
+      ];
+    },
+    itemClasses() {
+      return [
+        "hover:bg-gray-100",
+        "text-black",
+        "px-3",
+        "py-1",
+        "select-none",
+        "truncate",
+      ];
+    },
+    reportLinkClasses() {
+      return [
+        "w-full",
+        "inline-block",
+        "text-right",
+        "text-xs",
+        "italic",
+        "text-gray-500",
+        "hover:text-black",
+        "pr-2",
+      ];
+    },
+  },
+};
+</script>
+
+<style></style>
